82c35a893aa150c113968471d8d36739293c2862
jogeocc <jogeochanc@gmail.com>
2017-12-30 09:37:24 -0600

    Cambios de ejecucion d eboton

--- app/src/main/java/com/jgeo/www/miholamundoenjava/MainActivity.java
+++ app/src/main/java/com/jgeo/www/miholamundoenjava/MainActivity.java
@@ -1,9 +1,16 @@
 package com.jgeo.www.miholamundoenjava;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -12,27 +19,73 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.btnAcercaDe)
-    Button btnAcercaDe;
+
+    @BindView(R.id.etNombre)
+    EditText etNombre;
+    @BindView(R.id.etApellido)
+    EditText etApellido;
+    @BindView(R.id.etEmail)
+    EditText etEmail;
+    @BindView(R.id.etNumTele)
+    EditText etNumTele;
+    @BindView(R.id.stTerminos)
+    Switch stTerminos;
+    @BindView(R.id.btnGuardar)
+    Button btnGuardar;
     @BindView(R.id.btnSalir)
     Button btnSalir;
+    @BindView(R.id.contentMain)
+    ConstraintLayout contentMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
     }
 
-    @OnClick({R.id.btnAcercaDe, R.id.btnSalir})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.btnAcercaDe:
-                    Toast.makeText( this.getApplicationContext(), "Este es mi primer porgrama en andriod estudio con java",Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btnSalir:
-                System.exit(0);
-                break;
+
+    @OnClick(R.id.btnGuardar)
+    public void saveData() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+        if (stTerminos.isChecked()) {
+            Toast.makeText(this, "Guardar Contacto " + etNombre.getText().toString()
+                    + " " + etApellido.getText().toString() + ".", Toast.LENGTH_SHORT).show();
+        } else {
+            Snackbar.make(contentMain, "Debe de aceptar los termino y condiciones", Snackbar.LENGTH_LONG)
+                    .setAction("Ok", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            stTerminos.setChecked(true);
+                        }
+                    }).show();
+        }
+    }
+
+    @OnClick(R.id.btnSalvar)
+    public void onViewClicked() {
+        Log.d("entro:", "Entro el onclick del boton ");
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+        if (stTerminos.isChecked()) {
+            Toast.makeText(this, "Guardar Contacto " + etNombre.getText().toString()
+                    + " " + etApellido.getText().toString() + ".", Toast.LENGTH_SHORT).show();
+        } else {
+            Snackbar.make(contentMain, "Debe de aceptar los termino y condiciones", Snackbar.LENGTH_LONG)
+                    .setAction("Ok", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            stTerminos.setChecked(true);
+                        }
+                    }).show();
         }
     }
 }
